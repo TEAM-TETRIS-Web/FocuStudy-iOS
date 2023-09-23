091866196b474f2553b268d90af052c40f5b7f01
YugyeongChoi <xyukyeong@naver.com>
2023-09-23 18:14:57 +0900

    닉네임 화면 구현

--- FocuStudy/FocuStudy/NickNameView.swift
+++ FocuStudy/FocuStudy/NickNameView.swift
@@ -8,8 +8,42 @@
 import SwiftUI
 
 struct NickNameView: View {
+    @State var nickName: String = ""
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Spacer()
+            Spacer()
+            HStack(spacing : 0){
+                Text("닉네임")
+                    .font(.semiBold)
+                    .foregroundColor(.symbolBlue)
+                Text("을 설정해주세요.")
+                    .font(.semiBold)
+                    .foregroundColor(.black)
+                Spacer()
+            }
+            TextField("Enter your nickname", text: $nickName)
+              .padding()
+              .background(Color(uiColor: .secondarySystemBackground))
+            
+
+            Spacer()
+            HStack{
+                Spacer()
+                Text("\(nickName)(으)로 시작하기")
+                    .font(.bold16)
+                    .foregroundColor(.white)
+                    .padding(.vertical,9)
+                Spacer()
+            }
+            .background(Color.black)
+            .cornerRadius(12)
+            .padding(.bottom,100)
+            .padding(.horizontal,20)
+            Spacer()
+        }.padding(.horizontal,40)
+            .padding(.vertical,10)
     }
 }
 
